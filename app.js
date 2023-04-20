dcbbbe4092e9cf8eb66e13b9ffaf850dd5577d15
Vinicius Vaz <agvazvinicius@gmail.com>
2023-04-20 14:55:35 -0300

    indt fix

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const form = document.querySelector(".quiz-form");
 const finalResult = document.querySelector(".result");
 
-const correctAnswers = [
- "B", "A", "A",
- "D", "C", "B",
- "D", "B", "C",
- "A", "D", "B",
- "C", "C", "C"
-];
+const correctAnswers = ["B", "A", "A", "D", "C", "B", "D", "B", "C", "A", "D", "B", "C", "C", "C"];
 
 const showResults = (event) => {
 	event.preventDefault();
@@ -17,51 +11,51 @@ const showResults = (event) => {
 		form.inputQuestion2.value,
 		form.inputQuestion3.value,
 		form.inputQuestion4.value,
-    form.inputQuestion5.value,
+		form.inputQuestion5.value,
 		form.inputQuestion6.value,
 		form.inputQuestion7.value,
 		form.inputQuestion8.value,
-    form.inputQuestion9.value,
+		form.inputQuestion9.value,
 		form.inputQuestion10.value,
 		form.inputQuestion11.value,
 		form.inputQuestion12.value,
-    form.inputQuestion13.value,
-    form.inputQuestion14.value,
-    form.inputQuestion15.value,
+		form.inputQuestion13.value,
+		form.inputQuestion14.value,
+		form.inputQuestion15.value,
 	];
 
-  let score = 0;
-  
-  const addScore = (userAnswer, index) => {
+	let score = 0;
+
+	const addScore = (userAnswer, index) => {
 		if (userAnswer == correctAnswers[index]) {
 			score += 1;
-		} 
-	}
+		}
+	};
+
 	userAnswers.forEach(addScore);
 
 	scrollTo(0, 100);
 
 	finalResult.classList.remove("d-none");
 
-  
 	let counter = 0;
 	const scoreAnimation = setInterval(() => {
 		if (counter === score) {
 			clearInterval(scoreAnimation);
 		}
 
-    if (score < 15){
-      const tryAgain = document.querySelector('.tryAgainBtn')
+		if (score < 15) {
+			const tryAgain = document.querySelector(".tryAgainBtn");
 
-      tryAgain.classList.remove("d-none");
-      tryAgain.addEventListener('click', () =>{
-        location.reload();        
-      })
-    }
+			tryAgain.classList.remove("d-none");
+			tryAgain.addEventListener("click", () => {
+				location.reload();
+			});
+		}
 
 		finalResult.querySelector("span").textContent = `${counter} de 15`;
 		counter++;
 	}, 85);
 };
 
-form.addEventListener("submit", showResults)
+form.addEventListener("submit", showResults);
